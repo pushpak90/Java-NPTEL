7cd258427eeeaa9a4ef1c73e4f01e23521904eee
pushpak90 <pushpakafasate90@gmail.com>
2022-08-03 22:49:14 +0530

    Exercise 1_2

---
+++ Exercise1_2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercise1_2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int z = s.nextInt();
+        int result = 0;
+
+        if(x > y && x > z)
+        {
+            result = x;
+        }
+        else if(y > x && x > y)
+        {
+            result = y;
+        }
+        else if(z > x && z > y)
+        {
+            result = z;
+        }
+        else
+        {
+            result = x;
+        }
+
+        System.out.println(result);
+    }
+}
