75dc5df84dc8aabf43ebcf42912e0424c0926359
pushpak90 <pushpakafasate90@gmail.com>
2022-08-05 23:19:29 +0530

    Exercise1_4

--- Exercise1_4.java
+++ Exercise1_4.java
@@ -5,37 +5,28 @@ public class Exercise1_4 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int result = 0;
-        
-        //Use while loop check the number is Armstrong or not.
-        //store the output(1 or 0) in result variable.
 
-        int remainder, temp, count = 0,i;
-
+        // Use while loop check the number is Armstrong or not.
+        // store the output(1 or 0) in result variable.
+        int remainder, temp, count = 0, i;
         temp = n;
-        while(temp != 0)
-        {
+        while (temp != 0) {
             temp /= 10;
             count++;
         }
         i = count;
         temp = n;
-        while(count > 0)
-        {
+        while (count > 0) {
             remainder = temp % 10;
             result += Math.pow(remainder, i);
             temp /= 10;
             count--;
         }
-
-        if(n == temp)
-        {
+        if (n == result)
             result = 1;
-        }
         else
-        {
             result = 0;
-        }
-        System.out.println(result);
 
+        System.out.print(result);
     }
 }
