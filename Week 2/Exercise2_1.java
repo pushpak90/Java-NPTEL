8c8d3e73fb7474a2eacb5d07aafa376553edfb7b
pushpak90 <pushpakafasate90@gmail.com>
2022-08-11 21:43:59 +0530

    Week 2 Exercise 2_1

---
+++ Week 2/Exercise2_1.java
@@ -0,0 +1,23 @@
+class School { 
+    // This is a method in class School
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+} 
+// This is the class named Student
+class Student { 
+	// This is a method in class Student
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT"); 
+    } 
+}
+
+public class Exercise2_1{ 
+    public static void main(String args[]){
+        Student s1 = new Student();
+        s1.print();
+
+        School s2 = new School();
+        s1.print();
+    }
+}
