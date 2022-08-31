aab6c7e2104b9ac38769e6785b0a76d9cbb02cc4
pushpak90 <64779040+pushpak90@users.noreply.github.com>
2022-08-31 14:00:53 +0530

    Exercise6_1

--- Week 6/Exercise6_1/Exercise6_1.java
+++ Week 6/Exercise6_1/Exercise6_1.java
@@ -1,8 +1,8 @@
-public class Exercise6_1 extends Thread{
-    public void run()
-    {
+public class Exercise6_1 extends Thread {
+    public void run() {
         System.out.println("Thread is Running.");
     }
+
     public static void main(String[] args) {
         Exercise6_1 thread = new Exercise6_1();
         thread.start();
